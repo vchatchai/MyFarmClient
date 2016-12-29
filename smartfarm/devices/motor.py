df5bea9f082f2949e52fb31c898edca1319b91f4
chatchai vichai <chatchaivichai@192.168.1.101>
2016-12-29 22:26:00 +0700

    add function motor.

--- smartfarm/devices/motor.py
+++ smartfarm/devices/motor.py
@@ -2,26 +2,66 @@ from flask import Blueprint, render_template, abort
 from flask import Flask, jsonify
 from flask import Response
 from flask import json
-
+import wiringpi
 
 # from jinja2 import TemplateNotFound
+
 motor = Blueprint('motor', __name__)
+
+# wiringpi.wiringPiSetupSys()
+# wiringpi.wiringPiSetup()
+
+PORT = 10
+
+data = {
+'name'  : "",
+'status' : True
+}
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(PORT,1)
+wiringpi.digitalWrite(PORT,not data['status'])
+print("Motor started!")
+
+
  
 @motor.route('/motor/<name>')
 def status(name):
-# return "Test motsor by " + name
-#     try:
-#         return render_template('pages/%s.html' % page)
-#     except TemplateNotFound:
-#         abort(404)
-    data = {
-    'hello'  : name,
-    'number' : 3
-    }
+    data['name']= name
     js = json.dumps(data)
-    
     resp = Response(js, status=200, mimetype='application/json')
     resp.headers['Link'] = 'http://luisrei.com'
     
     return resp
+@motor.route('/motor/<name>/on')
+def on(name):
+    data['name']= name
+    data['status'] = True
+    wiringpi.digitalWrite(PORT,not data['status'])
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    return resp
+
+@motor.route('/motor/<name>/off')
+def off(name):
+    data['name']= name
+    data['status'] = 0
+    wiringpi.digitalWrite(PORT,not data['status'])
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    
+    return resp
+
+@motor.route('/motor/<name>/toggle')
+def toggle(name):
+    data['name']= name
+    data['status'] = not data['status']
+    wiringpi.digitalWrite(PORT,not data['status'])
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    
+    return resp
+
 
--- smartfarm/smart.py
+++ smartfarm/smart.py
@@ -5,18 +5,19 @@ Created on Dec 14, 2559 BE
 '''
 
 from flask import Flask
-
 from devices.example import example
 from devices.motor import motor
+
 
 
 app = Flask(__name__)
 # app.register_blueprint(example)
 app.register_blueprint(motor)
+a
 
 @app.route('/articles')
 def api_articles():
     return 'Test ' 
 
 if __name__ == '__main__':
-    app.run();
+    app.run("0.0.0.0",debug=True)

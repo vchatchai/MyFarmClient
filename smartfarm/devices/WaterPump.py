9dbc0b2b1364eb216ad7aa122113eef6b929261e
chatchai vichai <chatchaivichai@chatchais-MacBook-Pro.local>
2017-05-29 23:16:19 +0700

    change lib.

--- smartfarm/devices/WaterPump.py
+++ smartfarm/devices/WaterPump.py
@@ -1,7 +1,8 @@
 #from devices.Pump import Pump
 #import wiringpi
 import paho.mqtt.client as mqtt
-
+from pyA20.gpio import gpio
+from pyA20.gpio import port
 class WaterPump:
     def __init__(self, name, client):
         '''
@@ -13,10 +14,14 @@ class WaterPump:
         # wiringpi.wiringPiSetupGpio()
         # wiringpi.pinMode(PORT,1)
         # wiringpi.digitalWrite(PORT,not data['status'])
+        gpio.init()
+        gpio.setcfg(port.PA10, gpio.OUTPUT)
     def on_message(self, client, userdata, msg):
         payload_string = msg.payload.decode('utf-8')
         print("Topic1111111: {}. Payload: {}".format(msg.topic, payload_string))
 #     wiringpi.digitalWrite(PORT,not data['status'])
+        status = gpio.input(port.PA10)
+        gpio.output(port.PA10, not status)
     def on(self):
         '''
         On Pump
